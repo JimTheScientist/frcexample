502854e8950788da820e5eae8628ea4c8a6a3402
JimTheScientist <aidannpratt@gmail.com>
2022-02-04 08:13:35 -0600

    getting rid of some code, added vendordeps

---
+++ src/main/java/net/jimthescientist/frcexample/FRCExample.java
@@ -0,0 +1,19 @@
+package net.jimthescientist.frcexample;
+
+import edu.wpi.first.wpilibj.RobotBase;
+
+import java.awt.*;
+
+public class FRCExample {
+    private FRCExample() {
+    }
+
+    /**
+     * Main initialization function. Do not perform any initialization here.
+     *
+     * <p>
+     * If you change your main robot class, change the parameter type.
+     */
+    public static void main(String... args) {
+    }
+}
